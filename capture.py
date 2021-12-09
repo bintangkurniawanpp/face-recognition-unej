c33a48b76d945fbad7351b801a3d23a9f0f0fe05
AbiNazha <abizar213@gmail.com>
2021-12-09 09:21:49 +0700

    add capture image

--- capture.py
+++ capture.py
@@ -4,16 +4,21 @@ import sys
 cpt = 0
 maxFrames = 100 # if you want 5 frames only.
 
-try:
-    vidStream = cv2.VideoCapture(0) # index of your camera
-except:
-    print("problem opening input stream")
-    sys.exit(1)
+video = cv2.VideoCapture(1)
 
-while cpt < maxFrames:
-    ret, frame = vidStream.read() # read frame and return code.
-    if not ret: # if return code is bad, abort.
-        sys.exit(0)
-    cv2.imshow("test window", frame) # show image in window
-    cv2.imwrite("dataset/abizar/image%04i.jpg" %cpt, frame)
-    cpt += 1
+while True:
+    ret,frame = video.read()
+    cv2.imshow('camera',frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    if key == ord('c'):
+        while cpt < maxFrames:
+            if not ret: # if return code is bad, abort.
+                sys.exit(0)
+            cv2.imshow('captured', frame)
+            cv2.imwrite("dataset/muchdor/image%04i.jpg" %cpt, frame)
+            cpt += 1
+    
+video.release()
+cv2.destroyAllWindows()
