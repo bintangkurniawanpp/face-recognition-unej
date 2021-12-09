09f2e44441b6de11a8db62041b1f60df680fd323
AbiNazha <abizar213@gmail.com>
2021-12-09 09:53:03 +0700

    edit

--- capture.py
+++ capture.py
@@ -1,11 +1,12 @@
 import cv2
 import sys
+from pathlib import Path
 
 cpt = 0
-maxFrames = 100 # if you want 5 frames only.
+maxFrames = 5 # if you want 5 frames only.
 
 video = cv2.VideoCapture(1)
-
+inp = input("Nama : ")
 while True:
     ret,frame = video.read()
     cv2.imshow('camera',frame)
@@ -13,12 +14,15 @@ while True:
     if key == ord('q'):
         break
     if key == ord('c'):
+        dir = Path("dataset/" + str(inp)).mkdir(parents=True, exist_ok=True)
         while cpt < maxFrames:
             if not ret: # if return code is bad, abort.
                 sys.exit(0)
+            
             cv2.imshow('captured', frame)
-            cv2.imwrite("dataset/muchdor/image%04i.jpg" %cpt, frame)
+            cv2.imwrite("dataset/" + str(inp) + "/image%04i.jpg" %cpt, frame)
             cpt += 1
+        
     
 video.release()
 cv2.destroyAllWindows()
