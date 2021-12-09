d45c24600010c2ab3a54ac0e304eda1c81bf8155
bintangkurniawanpp <nnew34004@gmail.com>
2021-12-09 13:33:29 +0700

    update

--- capture.py
+++ capture.py
@@ -1,19 +1,27 @@
 import cv2
 import sys
+from pathlib import Path
 
 cpt = 0
-maxFrames = 100 # if you want 5 frames only.
+maxFrames = 5 # if you want 5 frames only.
 
-try:
-    vidStream = cv2.VideoCapture(0) # index of your camera
-except:
-    print("problem opening input stream")
-    sys.exit(1)
+video = cv2.VideoCapture(0)
+inp = input("Input Nama : ")
+while True:
+    ret,frame = video.read()
+    cv2.imshow('camera',frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    if key == ord('c'):
+        dir = Path("dataset/" + str(inp)).mkdir(parents=True, exist_ok=True)
+        while cpt < maxFrames:
+            if not ret: # if return code is bad, abort.
+                sys.exit(0)
+            
+            cv2.imshow('captured', frame)
+            cv2.imwrite("dataset/" + str(inp) + "/image%04i.jpg" %cpt, frame)
+            cpt += 1
 
-while cpt < maxFrames:
-    ret, frame = vidStream.read() # read frame and return code.
-    if not ret: # if return code is bad, abort.
-        sys.exit(0)
-    cv2.imshow("test window", frame) # show image in window
-    cv2.imwrite("image%04i.jpg" %cpt, frame)
-    cpt += 1
+video.release()
+cv2.destroyAllWindows()
