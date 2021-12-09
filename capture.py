11dfce307f7fe5f2f06ce81e1b29e7500d044a96
AbiNazha <abizar213@gmail.com>
2021-12-09 13:39:46 +0700

    add

--- capture.py
+++ capture.py
@@ -2,10 +2,11 @@ import cv2
 import sys
 from pathlib import Path
 
-cpt = 0
-maxFrames = 5 # if you want 5 frames only.
-
-video = cv2.VideoCapture(1)
+cpt = 0  
+maxFrames = 1000
+video = cv2.VideoCapture(0)
+v = video.get(5)
+fps = video.get(cv2.CAP_PROP_FPS)
 inp = input("Input Nama : ")
 while True:
     ret,frame = video.read()
@@ -16,11 +17,11 @@ while True:
     if key == ord('c'):
         dir = Path("dataset/" + str(inp)).mkdir(parents=True, exist_ok=True)
         while cpt < maxFrames:
-            if not ret: # if return code is bad, abort.
+            if not ret:
                 sys.exit(0)
-            
             cv2.imshow('captured', frame)
             cv2.imwrite("dataset/" + str(inp) + "/image%04i.jpg" %cpt, frame)
+            
             cpt += 1
 
 video.release()
